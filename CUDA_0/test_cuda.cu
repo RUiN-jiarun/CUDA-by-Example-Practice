48e63cac828c0e53b96ca255ee4881962c0896aa
RUiN-jiarun <1052707477@qq.com>
2022-06-20 17:13:11 +0800

    init

---
+++ CUDA_0/test_cuda.cu
@@ -0,0 +1,22 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+__global__ void hello_world_from_gpu(void)
+{
+	printf("Hello World from GPU\n");
+	return;
+}
+
+void test()
+{
+	printf("Hello World from CPU\n");
+	hello_world_from_gpu << < 1, 5 >> > ();
+	cudaDeviceReset();
+}
+
+int main()
+{
+	test();
+	return 0;
+}
